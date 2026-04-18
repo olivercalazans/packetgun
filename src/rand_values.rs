9837de48be164dd6b6a29a5405b7f9ac25da2758
Oliver Calazans <oliver.calazans@gmail.com>
2026-04-18 01:58:42 -0300

    refac rand_values

--- src/rand_values.rs
+++ src/rand_values.rs
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2026 Oliver R. Calazans Jeronimo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org>.
+ */
+
 use std::net::Ipv4Addr;
 use rand::{Rng, rngs::ThreadRng, prelude::SliceRandom};
 use crate::utils::{Mac, Bssid};
@@ -53,109 +70,6 @@ impl RandomValues {
     pub fn random_mac(&mut self) -> Mac {
         let bytes = self.random_vec_u8_6();
         Mac::new(bytes)
-    }
-
-
-
-    #[inline]
-    pub fn random_bssid(&mut self) -> Bssid {
-        let bytes = self.random_vec_u8_6();
-        Bssid::new(bytes )
-    }
-
-
-
-    #[inline]
-    pub fn random_seq(&mut self) -> u16 {
-        self.rng.gen_range(1..4095)
-    }
-
-
-
-    pub fn random_case_inversion(&mut self, input: &str) -> String {
-        if input.is_empty() {
-            return String::new();
-        }
-
-        let chars: Vec<char> = input.chars().collect();
-        let total_chars      = chars.len();
-
-        let change_count = self.determine_change_count(total_chars);
-
-        if change_count == 0 {
-            return input.to_string();
-        }
-
-        let letter_indices: Vec<usize> = chars
-            .iter()
-            .enumerate()
-            .filter(|&(_, &c)| c.is_alphabetic())
-            .map(|(idx, _)| idx)
-            .collect();
-
-        if letter_indices.is_empty() {
-            return input.to_string();
-        }
-
-        let selected_indices: Vec<usize> = if letter_indices.len() <= change_count {
-            letter_indices
-        } else {
-            letter_indices
-                .choose_multiple(&mut self.rng, change_count)
-                .cloned()
-                .collect()
-        };
-
-        Self::invert_case_at_indices(&chars, &selected_indices)
-    }
-
-
-
-    fn determine_change_count(&mut self, total_chars: usize) -> usize {
-        match total_chars {
-            0 => 0,
-            1..=3 => {
-                if self.rng.gen_bool(0.5) { 1 } else { 0 }
-            }
-            4..=7 => {
-                self.rng.gen_range(1..=2)
-            }
-            8..=15 => {
-                self.rng.gen_range(2..=4.min(total_chars / 2))
-            }
-            16..=31 => {
-                let sqrt_based = (total_chars as f64).sqrt().ceil() as usize;
-                self.rng.gen_range(3..=sqrt_based.min(total_chars / 3))
-            }
-            _ => {
-                let log_based     = (total_chars as f64).log2().ceil() as usize;
-                let percent_based = total_chars / 10;
-                let max_changes   = log_based.max(percent_based).min(total_chars / 4);
-                self.rng.gen_range(5..=max_changes)
-            }
-        }
-    }
-
-
-
-    fn invert_case_at_indices(chars: &[char], indices: &[usize]) -> String {
-        let mut result = String::with_capacity(chars.len());
-
-        for (i, &c) in chars.iter().enumerate() {
-            if indices.contains(&i) {
-                if c.is_lowercase() {
-                    result.push_str(&c.to_uppercase().to_string());
-                } else if c.is_uppercase() {
-                    result.push_str(&c.to_lowercase().to_string());
-                } else {
-                    result.push(c);
-                }
-            } else {
-                result.push(c);
-            }
-        }
-
-        result
     }
 
 }
